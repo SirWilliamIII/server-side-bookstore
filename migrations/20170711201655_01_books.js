60613cff2e5e6b52cd487fbca5c3020b2a7be9fe
Will Carpenter <willcarpenteriii@gmail.com>
2017-07-11 21:02:36 -0600

    seeded database books table

--- migrations/20170711201655_01_books.js
+++ migrations/20170711201655_01_books.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = (knex, Promise) => {
+		return knex.schema.createTable('books', table => {
+				table.increments();
+				table.string('title');
+				table.text('description');
+				table.string('book_img_url');
+				table.text('author_names');
+				table.string('genre');
+				table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+				table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+		});
 };
 
 exports.down = function(knex, Promise) {
-  
+		return knex.schema.dropTable('books');
 };
